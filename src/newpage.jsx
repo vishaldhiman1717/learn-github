f01c13852d3eaaf1bcb5c35f155686d55251af2a
vishal <vishaldhiman7404@gmail.com>
2023-01-15 21:32:32 +0530

    ffrdr

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const newpage = () => {
-  return <div>newpage</div>;
+  return (
+    <div>
+      newpage Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
+      consectetur dolorem optio magnam necessitatibus ea maiores earum? Harum
+      sed amet ipsum, quibusdam labore iusto! Cupiditate unde exercitationem,
+      corporis ipsam et tenetur culpa quisquam ab praesentium magnam rem maxime,
+      doloremque soluta.
+    </div>
+  );
 };
 
 export default newpage;

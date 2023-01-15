4293eba608af687bcf9e23ea92893aa5eca87ea4
vishal <vishaldhiman7404@gmail.com>
2023-01-15 21:22:32 +0530

    fnkf

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -4,12 +4,6 @@ const newpage = () => {
   return (
     <div>
       newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Qui alias dolores ex
-      minus repellendus necessitatibus excepturi iusto reiciendis dolorum illum.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, eligendi
-      autem. Autem officiis asperiores, voluptate id quaerat voluptates maiores
-      harum repellat laboriosam consequatur consequuntur a dolores magnam fugit
-      cumque ipsum.{" "}
     </div>
   );
 };

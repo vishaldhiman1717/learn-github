6fde93977f7edd45e664add640a1be60ee519be1
vishal <vishaldhiman7404@gmail.com>
2023-01-15 20:37:30 +0530

    fff

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -2,17 +2,7 @@ import React from "react";
 
 const newpage = () => {
   return (
-    <div>
-      newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Accusantium velit totam maiores animi! Provident obcaecati ea labore dolor
-      accusantium nulla voluptatem veniam quasi incidunt! Doloribus facere enim
-      earum optio ab? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      Quasi est saepe quod itaque magnam architecto sequi odit necessitatibus
-      commodi. Eum voluptate reiciendis ab id libero incidunt veritatis
-      molestias perspiciatis nostrum! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Deleniti beatae quis a, ratione dolorum esse ab aliquam
-      vitae accusamus veritatis.
-    </div>
+    <div>newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
   );
 };
 

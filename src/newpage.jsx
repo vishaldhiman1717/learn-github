f561f71cd874095110b3da687ebea2de6a1e6ff7
vishal <vishaldhiman7404@gmail.com>
2023-01-15 20:11:03 +0530

    second

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const newpage = () => {
-  return <div>newpage</div>;
+  return (
+    <div>
+      newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Accusantium velit totam maiores animi! Provident obcaecati ea labore dolor
+      accusantium nulla voluptatem veniam quasi incidunt! Doloribus facere enim
+      earum optio ab?
+    </div>
+  );
 };
 
 export default newpage;

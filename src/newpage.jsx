8116998acec0ccde0ac0b43aa244b40fb5cc930d
vishal <vishaldhiman7404@gmail.com>
2023-01-15 20:39:52 +0530

    ggg

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const newpage = () => {
   return (
-    <div>newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+    <div>
+      newpage Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Qui alias dolores ex
+      minus repellendus necessitatibus excepturi iusto reiciendis dolorum illum.
+    </div>
   );
 };
 

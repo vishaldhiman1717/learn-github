7066b177bed679fee136adf791eaa12d2000e407
vishal <vishaldhiman7404@gmail.com>
2023-01-15 21:29:17 +0530

    ff

--- src/newpage.jsx
+++ src/newpage.jsx
@@ -7,7 +7,10 @@ const newpage = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minima,
       nobis hic officia numquam vel expedita amet, sequi corrupti nihil est
       architecto sit! Nam laudantium, vel necessitatibus doloremque inventore
-      eos.
+      eos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+      pariatur eveniet quis! Reprehenderit accusantium tempore ducimus
+      laboriosam facere, quibusdam nostrum quidem, a accusamus vero debitis
+      molestiae consectetur voluptate nemo vitae itaque? Perferendis, sunt.
     </div>
   );
 };
